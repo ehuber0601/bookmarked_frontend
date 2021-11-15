03266a813aae425d6ba458709f4320bb33999f56
Eric Huberman <ehuber0601@gmail.com>
2021-11-15 15:04:57 -0500

    Finished project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import {Link} from "react-router-dom"
 
 const Header = (props) => {
-    return (
-        <nav className="nav">
-            <Link to="/">
-                <div>Bookmark App</div>
-            </Link>
-        </nav>
+    return (<header class="mdc-top-app-bar">
+        <div class="mdc-top-app-bar__row">
+        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+            <span class="mdc-top-app-bar__title"><Link to="/">Bookmarkd</Link></span>
+        </section>
+        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
+        </section>
+        </div>
+    </header>
     )
 }
 
